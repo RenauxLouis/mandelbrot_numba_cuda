4d0a3ab87ef13995b162a031aafca40721a7e1c3
jacky <renauxlouis@gmail.com>
2020-06-25 00:50:32 -0400

    clean

--- run_mandelbrot.py
+++ run_mandelbrot.py
@@ -5,41 +5,28 @@ from matplotlib.patches import Rectangle
 from matplotlib.widgets import RectangleSelector
 from numba import cuda
 from pylab import cm as cm
+import argparse
 
-global N_pixel, x1_coo, y1_coo, N_coo, L, M, power
-L = 800
-N_pixel = 1024
-ratio_image = 1.
-x1_coo = -1.5
-y1_coo = -1.5
-N_coo = 3.0
-i_cmap = 49
-power = 2
-fig = plt.figure(figsize=(12, 12))
-fig.suptitle("Interactive Mandelbrot Set Accelerated using Numba")
-ax = fig.add_subplot(111)
-cmaps = [m for m in cm.datad if not m.endswith("_r")]
-
-blockdim = (32, 8)
-griddim = (32, 16)
+BLOCKDIM = (32, 8)
+GRIDDIM = (32, 16)
 
 
 @numba.jit
-def mandel(x, y):
+def mandel(x, y, iters):
     c = complex(x, y)
     z = 0.0j
-    for i in range(L):
+    for i in range(iters):
         z = z*z + c
         if (z.real*z.real + z.imag*z.imag) >= 4:
             return i
-    return L
+    return iters
 
 
 mandel_gpu = cuda.jit(device=True)(mandel)
 
 
 @cuda.jit
-def mandel_kernel(x_0, y_0, N_coo, d_image):
+def mandel_kernel(x_0, y_0, N_coo, d_image, N_pixel, iters):
     pixel_size_x = N_coo / N_pixel
     pixel_size_y = N_coo / N_pixel
 
@@ -51,7 +38,7 @@ def mandel_kernel(x_0, y_0, N_coo, d_image):
         real = x_0 + x * pixel_size_x
         for y in range(startY, N_pixel, gridY):
             imag = y_0 + y * pixel_size_y
-            d_image[y, x] = mandel_gpu(real, imag)
+            d_image[y, x] = mandel_gpu(real, imag, iters)
 
 
 def zoom_on_square(eclick, erelease):
@@ -66,11 +53,11 @@ def zoom_on_square(eclick, erelease):
     H = (y2_pixel - y1_pixel)
     W = (x2_pixel - x1_pixel)
     ratio_h_w = H / W
-    if ratio_h_w > ratio_image:
+    if ratio_h_w > 1:
         difference = (H - W) / 2
         y1_pixel = y1_pixel + difference
         y2_pixel = y2_pixel - difference
-    elif ratio_h_w <= ratio_image:
+    elif ratio_h_w <= 1:
         difference = (W - H) / 2
         x1_pixel = x1_pixel + difference
         x2_pixel = x2_pixel - difference
@@ -86,35 +73,37 @@ def zoom_on_square(eclick, erelease):
 
     # Compute the mandelbrot set
     M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-    plot_image(M, N_coo, x1_coo, y1_coo)
+    plot_image(M, N_coo, x1_coo, y1_coo, N_pixel, iters)
 
     myobj = plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
     myobj.set_data(M)
     ax.add_patch(Rectangle((1 - .1, 1 - .1), 0.2, 0.2,
                            alpha=1, facecolor="none", fill=None, ))
-    ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
-                 (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
+    ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, iters=%d" %
+                 (N_coo, x1_coo, y1_coo, cmaps[i_cmap], iters))
     plt.draw()
 
 
 def zoom_on_point(event):
-    global N_pixel, N_coo, x1_coo, y1_coo, myobj, L, M, i_cmap, power
+    global N_pixel, N_coo, x1_coo, y1_coo, myobj, iters, M, i_cmap, power
+
+    # Zoom on clicked point; new N_coo=10% of old N_coo
     if event.button == 3 and event.inaxes:
-        # Zoom on clicked point; new N_coo=10% of old N_coo
         x1_pixel, y1_pixel = event.xdata, event.ydata
         x1_coo = x1_coo+N_coo*(x1_pixel-N_pixel/2.)/N_pixel
         y1_coo = y1_coo+N_coo*(y1_pixel-N_pixel/2.)/N_pixel
         N_coo = N_coo*.1
 
         M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-        plot_image(M, N_coo, x1_coo, y1_coo)
+        plot_image(M, N_coo, x1_coo, y1_coo, N_pixel, iters)
 
         myobj = plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
-        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
-                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
+        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, iters=%d" %
+                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], iters))
         plt.draw()
+
+    # Click on left N_coo of image to reset to full fractal
     if not event.inaxes and event.x < .3*N_pixel:
-        # Click on left N_coo of image to reset to full fractal
         power = 2
         N_coo = 3.0
         x1_coo = -.5
@@ -122,12 +111,13 @@ def zoom_on_point(event):
         i_cmap = 49
 
         M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-        plot_image(M, N_coo, x1_coo, y1_coo)
+        plot_image(M, N_coo, x1_coo, y1_coo, N_pixel, iters)
 
         myobj = plt.imshow(M, cmap=cmaps[i_cmap], origin="lower")
-        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
-                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
+        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, iters=%d" %
+                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], iters))
         plt.draw()
+
     # Left click on right N_coo of image to set a random colormap
     if event.button == 1 and not event.inaxes and event.x > .7*N_pixel:
         i_cmap_current = i_cmap
@@ -137,35 +127,74 @@ def zoom_on_point(event):
             if i_cmap < 0:
                 i_cmap = len(cmaps)-1
         myobj = plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
-        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
-                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
+        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, iters=%d" %
+                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], iters))
         plt.draw()
     # Right click on right N_coo to set colormap="flag"
     if event.button == 3 and not event.inaxes and event.x > .7*N_pixel:
         i_cmap = 49
         myobj = plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
-        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
-                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
+        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, iters=%d" %
+                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], iters))
         plt.draw()
 
 
-zoom_on_point.RS = RectangleSelector(
-    ax, zoom_on_square, drawtype="box", useblit=True, button=[1, 3],
-    minspanx=5, minspany=5, spancoords="pixels")
-
-def plot_image(M, N_coo, x1_coo, y1_coo):
+def plot_image(M, N_coo, x1_coo, y1_coo, N_pixel, iters):
     d_image = cuda.to_device(M)
-    mandel_kernel[griddim, blockdim](x1_coo, y1_coo, N_coo, d_image)
+    mandel_kernel[GRIDDIM, BLOCKDIM](x1_coo, y1_coo, N_coo, d_image, N_pixel, iters)
     d_image.to_host()
 
 
-fig.canvas.mpl_connect("button_press_event", zoom_on_point)
-fig.canvas.mpl_connect("key_press_event", key_selector)
+def main(iters, N_pixel, x1_coo, y1_coo, N_coo, i_cmap, power):
+    global cmaps, ax
 
-M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-plot_image(M, N_coo, x1_coo, y1_coo)
+    fig = plt.figure(figsize=(12, 12))
+    fig.suptitle("Interactive Mandelbrot Set Accelerated using Numba")
+    ax = fig.add_subplot(111)
+    cmaps = [m for m in cm.datad if not m.endswith("_r")]
 
-ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
-             (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
-plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
-plt.show()
+    zoom_on_point.RS = RectangleSelector(
+        ax, zoom_on_square, drawtype="box", useblit=True, button=[1, 3],
+        minspanx=5, minspany=5, spancoords="pixels")
+
+    fig.canvas.mpl_connect("button_press_event", zoom_on_point)
+
+    M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
+    plot_image(M, N_coo, x1_coo, y1_coo, N_pixel, iters)
+
+    ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, iters=%d" %
+                (N_coo, x1_coo, y1_coo, cmaps[i_cmap], iters))
+    plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
+    plt.show()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Preprocessing Pipeline")
+    parser.add_argument("--iters", type=int, default=800)
+    parser.add_argument("--N_pixel", type=int, default=1024)
+    parser.add_argument("--x1_coo", type=float, default=-1.5)
+    parser.add_argument("--y1_coo", type=float, default=-1.5)
+    parser.add_argument("--N_coo", type=float, default=3.)
+    parser.add_argument("--i_cmap", type=int, default=49)
+    parser.add_argument("--power", type=int, default=2)
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    global N_coo, N_pixel, x1_coo, y1_coo, iters
+
+    args = parse_args()
+    N_coo = args.N_coo
+    N_pixel = args.N_pixel
+    x1_coo = args.x1_coo
+    y1_coo = args.y1_coo
+    iters = args.iters
+    i_cmap = args.i_cmap
+    power = args.power
+
+    main(iters=iters, N_pixel=N_pixel,
+         x1_coo=x1_coo, y1_coo=y1_coo, N_coo=N_coo,
+         i_cmap=i_cmap, power=power)
