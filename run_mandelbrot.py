b46cb05f5ddafbe9da84a21275ce86e1592f143a
jacky <renauxlouis@gmail.com>
2020-06-24 23:37:13 -0400

    remove unused commands

--- run_mandelbrot.py
+++ run_mandelbrot.py
@@ -16,7 +16,7 @@ N_coo = 3.0
 i_cmap = 49
 power = 2
 fig = plt.figure(figsize=(12, 12))
-fig.suptitle('Interactive Mandelbrot Set Accelerated using Numba')
+fig.suptitle("Interactive Mandelbrot Set Accelerated using Numba")
 ax = fig.add_subplot(111)
 cmaps = [m for m in cm.datad if not m.endswith("_r")]
 
@@ -39,7 +39,6 @@ mandel_gpu = cuda.jit(device=True)(mandel)
 
 
 @cuda.jit
-# def mandel_kernel(min_x, max_x, min_y, max_y, image):
 def mandel_kernel(x_0, y_0, N_coo, d_image):
     pixel_size_x = N_coo / N_pixel
     pixel_size_y = N_coo / N_pixel
@@ -56,7 +55,6 @@ def mandel_kernel(x_0, y_0, N_coo, d_image):
 
 
 def zoom_on_square(eclick, erelease):
-    'eclick and erelease are the press and release events'
     global N_pixel, N_coo, x1_coo, y1_coo, myobj, M, power
 
     x1_pixel, y1_pixel = min(eclick.xdata, erelease.xdata), min(
@@ -88,90 +86,19 @@ def zoom_on_square(eclick, erelease):
 
     # Compute the mandelbrot set
     M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-    plot_image(M)
-    myobj = plt.imshow(M, origin='lower', cmap=cmaps[i_cmap])
+    plot_image(M, N_coo, x1_coo, y1_coo)
+
+    myobj = plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
     myobj.set_data(M)
     ax.add_patch(Rectangle((1 - .1, 1 - .1), 0.2, 0.2,
-                           alpha=1, facecolor='none', fill=None, ))
-    ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
+                           alpha=1, facecolor="none", fill=None, ))
+    ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
                  (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
     plt.draw()
 
 
-def key_selector(event):
-    global N_pixel, N_coo, x1_coo, y1_coo, myobj, M, power, L, i_cmap
-    # print(' Key pressed.')
-    if event.key == u'up':  # Increase max number of iterations
-        L = int(L*1.2)
-
-        M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-        plot_image(M)
-
-        myobj = plt.imshow(M, cmap=cmaps[i_cmap], origin='lower')
-        ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
-                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
-        plt.draw()
-    if event.key == u'down':  # Decrease max number of iterations
-        L = int(L/1.2)
-
-        M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-        plot_image(M)
-
-        myobj = plt.imshow(M, cmap=cmaps[i_cmap], origin='lower')
-        ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
-                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
-        plt.draw()
-    if event.key == u'right':  # Increase  number of pixels
-        N_pixel = int(N_pixel*1.2)
-
-        M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-        plot_image(M)
-
-        myobj = plt.imshow(M, cmap=cmaps[i_cmap], origin='lower')
-        ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
-                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
-        plt.draw()
-    if event.key == u'left':  # Decrease  number of pixels
-        N_pixel = int(N_pixel/1.2)
-
-        M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-        plot_image(M)
-
-        myobj = plt.imshow(M, cmap=cmaps[i_cmap], origin='lower')
-        ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
-                     (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
-        plt.draw()
-    if event.key in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        # Decrease  number of pixels
-        power = int(event.key)
-        if power < 10 and power > 0:
-            print("Power index set to %d" % power)
-            i_cmap = 49
-            N_coo = 3.0
-            x1_coo = -.5
-            y1_coo = 0.
-            L = 200
-
-            M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-            plot_image(M)
-
-            myobj = plt.imshow(M, cmap=cmaps[i_cmap], origin='lower')
-            ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
-                         (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
-            plt.draw()
-
-
-key_selector.RS = RectangleSelector(ax, zoom_on_square,
-                                    drawtype='box', useblit=True,
-                                    button=[1, 3],  # don't use middle button
-                                    minspanx=5, minspany=5,
-                                    spancoords='pixels')
-
-
 def zoom_on_point(event):
     global N_pixel, N_coo, x1_coo, y1_coo, myobj, L, M, i_cmap, power
-    # print(" Button pressed: %d" % (event.button))
-    # print(' event.x= %f, event.y= %f '%(event.x,event.y))
     if event.button == 3 and event.inaxes:
         # Zoom on clicked point; new N_coo=10% of old N_coo
         x1_pixel, y1_pixel = event.xdata, event.ydata
@@ -180,10 +107,10 @@ def zoom_on_point(event):
         N_coo = N_coo*.1
 
         M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-        plot_image(M)
+        plot_image(M, N_coo, x1_coo, y1_coo)
 
-        myobj = plt.imshow(M, origin='lower', cmap=cmaps[i_cmap])
-        ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
+        myobj = plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
+        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
                      (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
         plt.draw()
     if not event.inaxes and event.x < .3*N_pixel:
@@ -195,10 +122,10 @@ def zoom_on_point(event):
         i_cmap = 49
 
         M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-        plot_image()
+        plot_image(M, N_coo, x1_coo, y1_coo)
 
-        myobj = plt.imshow(M, cmap=cmaps[i_cmap], origin='lower')
-        ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
+        myobj = plt.imshow(M, cmap=cmaps[i_cmap], origin="lower")
+        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
                      (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
         plt.draw()
     # Left click on right N_coo of image to set a random colormap
@@ -209,34 +136,36 @@ def zoom_on_point(event):
             i_cmap -= 1
             if i_cmap < 0:
                 i_cmap = len(cmaps)-1
-        myobj = plt.imshow(M, origin='lower', cmap=cmaps[i_cmap])
-        ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
+        myobj = plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
+        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
                      (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
         plt.draw()
-    # Right click on right N_coo to set mapolormap='flag'
+    # Right click on right N_coo to set colormap="flag"
     if event.button == 3 and not event.inaxes and event.x > .7*N_pixel:
         i_cmap = 49
-        myobj = plt.imshow(M, origin='lower', cmap=cmaps[i_cmap])
-        ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
+        myobj = plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
+        ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
                      (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
         plt.draw()
 
 
-def plot_image(M):
-    global N_coo, x1_coo, y1_coo
+zoom_on_point.RS = RectangleSelector(
+    ax, zoom_on_square, drawtype="box", useblit=True, button=[1, 3],
+    minspanx=5, minspany=5, spancoords="pixels")
 
+def plot_image(M, N_coo, x1_coo, y1_coo):
     d_image = cuda.to_device(M)
     mandel_kernel[griddim, blockdim](x1_coo, y1_coo, N_coo, d_image)
     d_image.to_host()
 
 
-fig.canvas.mpl_connect('button_press_event', zoom_on_point)
-fig.canvas.mpl_connect('key_press_event', key_selector)
+fig.canvas.mpl_connect("button_press_event", zoom_on_point)
+fig.canvas.mpl_connect("key_press_event", key_selector)
 
 M = np.zeros((N_pixel, N_pixel), dtype=np.uint8)
-plot_image(M)
+plot_image(M, N_coo, x1_coo, y1_coo)
 
-ax.set_title('Side=%.2e, x=%.2e, y=%.2e, %s, L=%d' %
+ax.set_title("Side=%.2e, x=%.2e, y=%.2e, %s, L=%d" %
              (N_coo, x1_coo, y1_coo, cmaps[i_cmap], L))
-plt.imshow(M, origin='lower', cmap=cmaps[i_cmap])
+plt.imshow(M, origin="lower", cmap=cmaps[i_cmap])
 plt.show()
